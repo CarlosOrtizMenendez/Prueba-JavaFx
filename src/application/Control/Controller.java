ddb33c04ff4cb74ac1d6d7e511ceedc08b363933
21646436 <21646436@C306-26.alumnos.uem.es>
2017-12-05 17:55:49 +0100

    Commit vista 1.5

--- src/application/Control/Controller.java
+++ src/application/Control/Controller.java
@@ -1,5 +1,39 @@
 package application.Control;
 
-public class Controller {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Controller implements Initializable{
+
+	@FXML
+	private JFXButton boton;
+	@Override
+	public void initialize(URL location, ResourceBundle resource) {
+		// TODO Auto-generated method stub
+		/*
+		boton.setOnMouseClicked(event -> {
+			 FXMLLoader loader = new FXMLLoader();
+	            loader.setLocation(Main.class.getResource("vista/FreeSolo.fxml"));
+	            try {
+					AnchorPane page = (AnchorPane) loader.load();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	            Stage sendStage = new Stage();
+	            sendStage.setTitle("Superheader");
+		});
+		*/
+	}
+	
 }
